f7d9c1972bb33b6e22dc66fa97833c2182470c68
Nevezhin Yan <yaniknezhin@gmail.com>
2023-12-25 05:24:19 +0600

    Solving every tasks

--- assignment1/src/main.py
+++ assignment1/src/main.py
@@ -1,35 +1,8 @@
 from customException import CustomException
 from tasks import customInput
+from runClass import Run
 import importlib
 import sys
-
-
-class Run:
-    numberOfTask: int = None
-
-
-    def run(self, msvFunctions):
-        try:
-            if self.numberOfTask == None:
-                numberOfTask: int = int(input("Введите номер задачи: "))
-                if (numberOfTask < 0 or numberOfTask > 25) and numberOfTask != 32 and numberOfTask != 33:
-                    raise CustomException("Номер задачи от 1 до 25 (или 32/33) ...")
-                if numberOfTask == 32 or numberOfTask == 33: numberOfTask -= 6
-                self.numberOfTask = numberOfTask
-            func = msvFunctions[ self.numberOfTask - 1]
-            func()
-            isException = False
-        except ValueError:
-            raise CustomException("Номер задачи - это номер, а не число.")
-        except CustomException as e:
-            raise e
-        except ZeroDivisionError:
-            CustomException("Другалек на ноль делит")
-        except Exception as e:
-            print("Чет походу я внатуре неправильно одно из заданий написал ;(...")
-            print(e.__class__.__name__)
-            sys.exit(0)
-
 
 def returnMsvOfFunction():
     file_name = "tasks"
