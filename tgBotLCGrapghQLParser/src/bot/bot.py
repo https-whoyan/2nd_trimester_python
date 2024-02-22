5455fb4ca211de74b13290c79231a451643c5ea0
Nevezhin Yan <yaniknezhin@gmail.com>
2024-02-22 15:21:24 +0600

    Telegram Bot done

---
+++ tgBotLCGrapghQLParser/src/bot/bot.py
@@ -0,0 +1,99 @@
+import asyncio
+import logging
+import sys
+
+from typing import List
+
+from aiogram.enums import ParseMode
+from config import BOT_TOKEN
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+from src.parser.parser import getLastACSubmissions
+
+from schemas import User
+from src.database.utils import (
+    insertUserToDB,
+    containUserInDB,
+    getUsernameFromUserID
+)
+from utils import (
+    validateToUserSchema,
+    newHandleUsersStanding,
+    getMessageAboutChangingLCHandler,
+    getMessageAboutSubmissions
+)
+
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    currUser: User = validateToUserSchema(message.from_user)
+    if not containUserInDB(currUser.id):
+        insertUserToDB(currUser)
+        await message.answer(
+            f"Привет, {hbold(message.from_user.full_name)}. Этот бот умеет сообщать тебе информацию"
+            f" о твоих последних 5-ти успешных решенных задач, который ты"
+            f" решил на сайте {hbold('Leetcode.com')}. \n<b>Для начала укажи свой LC handle.</b>"
+            f" Это можно сделать командой /setLCHandle"
+        )
+    else:
+        await message.answer("Че ты, какой старт?))) Ты ботом уже пользовался. У меня есть только 2 команды: \n"
+                             "/setLCHandle \n/viewLCSubmissions")
+
+
+async def setLCHandleCommandHandler(message: Message) -> None:
+    await message.answer(
+        f"Пришли свой LCHandle сообщением ниже. Его можно узнать из nickname'а у тебя на странице, или из ссылки, "
+        f"при прогрузки твоей странице на сайте {hbold('Leetcode.com')}. \nЯ посчитаю его правильным, "
+        f"если при получении html страницы с твоим профилем мне не "
+        f"выдаст ошибку 404: Страница не найдена. \n\n<b>Пример: https_whoyan</b>"
+    )
+    newHandleUsersStanding.add(message.from_user.id)
+
+
+@dp.message()
+async def message_handle(message: Message):
+    if message.text == '/setLCHandle':
+        await setLCHandleCommandHandler(message)
+    elif message.text == '/viewLCSubmissions':
+        await message.answer("Получаю данные по последним отправкам:...")
+        usernameFromDB: str = getUsernameFromUserID(message.from_user.id)
+        if len(usernameFromDB) == 0:
+            await message.answer(f"Твоего хэндла нету в базе данных. Пожалуйста, добавь его,"
+                                 f"командой {hbold('/setLCHandle')}")
+            return
+        # Парсинг данных
+        await getLastACSubmissions(
+            message.from_user.id,
+            usernameFromDB
+        )
+        InfoAboutMessageToUser: List[str] = getMessageAboutSubmissions(message.from_user.id)
+        messageToUser: str = InfoAboutMessageToUser[0]
+        parseModeInfo: str = InfoAboutMessageToUser[1]
+        await message.answer(messageToUser, parse_mode=parseModeInfo)
+    elif message.from_user.id in newHandleUsersStanding:
+        currUser: User = validateToUserSchema(message.from_user)
+        if not containUserInDB(currUser.id):
+            insertUserToDB(currUser)
+        newHandleUsersStanding.remove(message.from_user.id)
+        messageToUser: str = getMessageAboutChangingLCHandler(message)
+        await message.answer(messageToUser)
+    else:
+        await message.answer("Я не знаю, что ты пишешь)) У меня есть только 2 команды: \n"
+                             "/setLCHandle \n/viewLCSubmissions")
+
+
+async def main() -> None:
+    bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
---
+++ tgBotLCGrapghQLParser/src/bot/utils.py
@@ -0,0 +1,100 @@
+from typing import List
+
+from aiogram.types import Message
+from aiogram.types.user import User as tgTypeUser
+from aiogram.utils.markdown import hbold
+from src.bot.schemas import User
+
+from src.parser.schemas import LCSubmission
+
+from src.database.utils import updateLCHandle, getLastSubmissions
+
+import requests
+
+
+newHandleUsersStanding = set()
+
+
+def validateToUserSchema(tgUser: tgTypeUser) -> User:
+    try:
+        return User(
+            tgUser.id,
+            lc_handle=""
+        )
+    except Exception:
+        return None
+
+
+def getMessageAboutChangingLCHandler(message: Message) -> str:
+    messageLCHandle = message.text
+    if not isCorrectLCHandle(messageLCHandle):
+        return f"Хэндл {hbold(messageLCHandle)} не валидный. Пожалуйста, отправьте его, согласно инстукции"
+    updateLCHandle(message.from_user.id, messageLCHandle)
+    htmlLink: str = f"https://leetcode.com/{messageLCHandle}"
+    messageToUser: str = (f"Ваш хэндл изменен на {messageLCHandle}. Согласно, хэндлу, ваша страница: {hbold(htmlLink)}"
+                          f"\n<b>Что бы просмотреть последние решенные задачи на LeetCode.com, "
+                          f"воспользуйтесь командой <u>/viewLCSubmissions</u></b>")
+    return messageToUser
+
+
+def isCorrectLCHandle(lcHadle: str) -> bool:
+    try:
+        testURL = f"https://leetcode.com/{lcHadle}"
+        response = requests.get(testURL)
+        return response.status_code == 200
+    except Exception:
+        return False
+
+
+def getVisualDiffSymbol(difficulty: str) -> str:
+    if difficulty == "Easy":
+        return "🟢"
+    elif difficulty == "Medium":
+        return "🟡"
+    return "🔴"
+
+
+def getMessageAboutSubmissions(userID: int) -> [str]:
+    try:
+        lastUserSubmissions: List[LCSubmission] = getLastSubmissions(userID)
+        sumbissionsCount: int = len(lastUserSubmissions)
+
+        messageToUser: str
+        if sumbissionsCount == 0:
+            utilsMessage: str = "Кстати, вот тебе решение Golang'e:"
+            messageToUser = (f"У тебя нету ни одной принятой Accept'ой задачи. Реши хотя бы TwoSum. \n\n"
+                             f"**{utilsMessage}** \n```go\n"
+                             f"func twoSum(nums []int, target int) []int {{\n"
+                             f"    mpVars := make(map[int]int)\n"
+                             f"    mpKeys := make(map[int]bool)\n"
+                             f"    for i, num := range nums {{\n"
+                             f"        if mpKeys[target - num] {{\n"
+                             f"            return []int{{mpVars[target - num], i}}\n"
+                             f"        }}\n"
+                             f"        mpKeys[num] = true\n"
+                             f"        mpVars[num] = i\n"
+                             f"    }}\n"
+                             f"    return []int{{}}\n"
+                             f"}}\n"
+                             f"```")
+            return [messageToUser, 'Markdown']
+        if sumbissionsCount == 1:
+            messageToUser = f"Твоя последняя попытка: \n\n"
+        elif sumbissionsCount != 5:
+            messageToUser = f"Твои последние {sumbissionsCount} попытки:\n\n"
+        else:
+            messageToUser = f"Твои последние {sumbissionsCount} попыткок:\n\n"
+        for sumbissionIndex, submission in enumerate(lastUserSubmissions):
+            typedSumbission: LCSubmission = submission
+            print(typedSumbission.date)
+            visualDiffSymbol: str = getVisualDiffSymbol(typedSumbission.task.difficulty)
+            messageToUser += (f"{hbold(sumbissionIndex + 1)}.: {hbold(typedSumbission.task.id)}."
+                              f"{hbold(typedSumbission.task.name)}\n"
+                              f"Сложность задачи: {hbold(typedSumbission.task.difficulty)} {visualDiffSymbol}\n"
+                              f"Время решения: {hbold(typedSumbission.date)}")
+            if sumbissionIndex != sumbissionsCount - 1:
+                messageToUser += "\n\n"
+
+        return [messageToUser, 'HTML']
+    except Exception as e:
+        return ["", ""]
